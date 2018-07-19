6ce7ee01baade5f1836f158591583ae95d7fc8fb
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-07-19 16:57:57 +0700

    Add more criterias for home screen

--- ScreenManager.py
+++ ScreenManager.py
@@ -5,17 +5,22 @@ from TemplateImage import TemplateImage
 
 class ScreenManager:
 
-    templateFolder = "ScreenTemplate"
-    defaultPrecisionThreadhold = 1000
-
     def __init__(self):
         self.templates = []
-        self.templates.append(TemplateImage(ScreenType.DEVICE_HOME, ScreenManager.templateFolder + "\DeviceHome.png"))
-        self.templates.append(TemplateImage(ScreenType.TAP_TO_START, ScreenManager.templateFolder + "\TapToStart.png"))
-        self.templates.append(TemplateImage(ScreenType.EVENT_INFO, ScreenManager.templateFolder + "\EventInfo.png", 120000))
-        self.templates.append(TemplateImage(ScreenType.DAILY_LOGIN_REWARD, ScreenManager.templateFolder + "\DailyLoginReward.png", 6000))
-        self.templates.append(TemplateImage(ScreenType.GAME_HOME, ScreenManager.templateFolder + "\GameHome.png"))
-        self.templates.append(TemplateImage(ScreenType.RESULT, ScreenManager.templateFolder + "\Result.png"))
+        self.templates.append(TemplateImage(ScreenType.DEVICE_HOME, "DeviceHome.png"))
+        self.templates.append(TemplateImage(ScreenType.TAP_TO_START, "TapToStart.png"))
+        self.templates.append(TemplateImage(ScreenType.EVENT_INFO, "EventInfo.png", 120000))
+        self.templates.append(TemplateImage(ScreenType.DAILY_LOGIN_REWARD, "DailyLoginReward.png", 6000))
+        
+        gameHome = TemplateImage(ScreenType.GAME_HOME, "GameHome_MainMenu.png", 30000000)
+        gameHome.AddCriteria("GameHome_EventMailChatButtons.png", 500000)
+        gameHome.AddCriteria("GameHome_Gold.png", 500000)
+        gameHome.AddCriteria("GameHome_MoonStone.png", 500000)
+        gameHome.AddCriteria("GameHome_Shoes.png", 500000)
+        gameHome.AddCriteria("GameHome_BattleTickets.png", 500000)
+        self.templates.append(gameHome)
+        
+        self.templates.append(TemplateImage(ScreenType.RESULT, "Result.png"))
 
     def GetScreen(self, screenShot):
         screenType = ScreenType.UNKNOWN
--- TemplateImage.py
+++ TemplateImage.py
@@ -1,22 +1,26 @@
 import cv2
+import os
 
 class TemplateImage:
 
-    def __init__(self, screenType, filePath, precision = 1000):
+    templateFolder = "ScreenTemplate"
+
+    def __init__(self, screenType, fileName, precision = 1000):
         self.screenType = screenType
         self.criterias = []
-        self.AddCriteria(filePath, precision)
+        self.AddCriteria(fileName, precision)
 
-    def AddCriteria(self, filePath, precision = 1000):
-        self.criterias.append([cv2.imread(filePath, 0), precision])
+    def AddCriteria(self, fileName, precision = 1000):
+        filePath = os.path.abspath(TemplateImage.templateFolder + "/" +  fileName)
+        self.criterias.append([fileName, cv2.imread(filePath, 0), precision])
 
     def FindMatch(self, screenShot):
         result = FindMatchResult(True, [0, 0])
         for criteria in self.criterias:
-            res = cv2.matchTemplate(screenShot.image, criteria[0], cv2.TM_SQDIFF)
+            res = cv2.matchTemplate(screenShot.image, criteria[1], cv2.TM_SQDIFF)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            print("[ScreenManager] IsMatch() " + str(self.screenType).ljust(30) + " " + str(min_val).rjust(15) + " " + str(min_val < criteria[1]))
-            if min_val < criteria[1]:
+            print("[ScreenManager] IsMatch() " + str(self.screenType).ljust(30) + " " + criteria[0].ljust(40) + " " + str(min_val).rjust(15) + " " + str(min_val < criteria[2]))
+            if min_val < criteria[2]:
                 result.location = min_loc
             else:
                 result.isMatch = False
