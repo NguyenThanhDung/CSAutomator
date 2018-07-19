629efe1cb7d1d2d3d0b179a0bbf2aff73e583cd5
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-07-19 17:01:36 +0700

    Change default precision

--- ScreenManager.py
+++ ScreenManager.py
@@ -7,20 +7,20 @@ class ScreenManager:
 
     def __init__(self):
         self.templates = []
-        self.templates.append(TemplateImage(ScreenType.DEVICE_HOME, "DeviceHome.png"))
-        self.templates.append(TemplateImage(ScreenType.TAP_TO_START, "TapToStart.png"))
+        self.templates.append(TemplateImage(ScreenType.DEVICE_HOME, "DeviceHome.png", 1000))
+        self.templates.append(TemplateImage(ScreenType.TAP_TO_START, "TapToStart.png", 1000))
         self.templates.append(TemplateImage(ScreenType.EVENT_INFO, "EventInfo.png", 120000))
         self.templates.append(TemplateImage(ScreenType.DAILY_LOGIN_REWARD, "DailyLoginReward.png", 6000))
         
         gameHome = TemplateImage(ScreenType.GAME_HOME, "GameHome_MainMenu.png", 30000000)
-        gameHome.AddCriteria("GameHome_EventMailChatButtons.png", 500000)
-        gameHome.AddCriteria("GameHome_Gold.png", 500000)
-        gameHome.AddCriteria("GameHome_MoonStone.png", 500000)
-        gameHome.AddCriteria("GameHome_Shoes.png", 500000)
-        gameHome.AddCriteria("GameHome_BattleTickets.png", 500000)
+        gameHome.AddCriteria("GameHome_EventMailChatButtons.png")
+        gameHome.AddCriteria("GameHome_Gold.png")
+        gameHome.AddCriteria("GameHome_MoonStone.png")
+        gameHome.AddCriteria("GameHome_Shoes.png")
+        gameHome.AddCriteria("GameHome_BattleTickets.png")
         self.templates.append(gameHome)
         
-        self.templates.append(TemplateImage(ScreenType.RESULT, "Result.png"))
+        self.templates.append(TemplateImage(ScreenType.RESULT, "Result.png", 1000))
 
     def GetScreen(self, screenShot):
         screenType = ScreenType.UNKNOWN
