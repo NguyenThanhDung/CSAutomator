90262aa6f51dab08e729bbdabf3c7abc70c97900
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-08-26 00:21:59 +0700

    Read sub stats

--- Equipment.py
+++ Equipment.py
@@ -8,17 +8,51 @@ class Equipment:
         screenshot = Image.fromarray(screen.image)
 
         equipmentArea = (385, 106, 763, 614)
-        equipmentImage = screenshot.crop(equipmentArea)
-        equipmentImage = equipmentImage.rotate(270, expand=1)
-        # equipmentImage.show()
-        # equipmentImage.save("Temp/Equipment.png")
+        self.equipmentImage = screenshot.crop(equipmentArea)
+        self.equipmentImage = self.equipmentImage.rotate(270, expand=1)
+        # self.equipmentImage.show()
+        # self.equipmentImage.save("Temp/Equipment.png")
 
-        mainStatArea = (161, 97, 488, 128)
-        mainStatImage = equipmentImage.crop(mainStatArea)
-        mainStatText = pytesseract.image_to_string(mainStatImage)
-        print("[Equipment] Main Stat: " + mainStatText)
+        mainStatText = self.GetMainStatText()
+        print("[Equipment] mainStatText: " + mainStatText)
+
+        subStatText = self.GetSubStatText(1)
+        if subStatText is not None:
+            print("[Equipment] subStatTextSlot1: " + subStatText)
+
+        subStatText = self.GetSubStatText(2)
+        if subStatText is not None:
+            print("[Equipment] subStatTextSlot2: " + subStatText)
+
+        subStatText = self.GetSubStatText(3)
+        if subStatText is not None:
+            print("[Equipment] subStatTextSlot3: " + subStatText)
+
+        subStatText = self.GetSubStatText(4)
+        if subStatText is not None:
+            print("[Equipment] subStatTextSlot4: " + subStatText)
 
         if "%" not in mainStatText:
             self.isGood = False
         else:
             print("[Equipment] Continue...")
+            self.isGood = True
+
+    def GetMainStatText(self):
+        return self.ImageToString((161, 97, 488, 128))
+
+    def GetSubStatText(self, slot):
+        if slot == 1:
+            return self.ImageToString((161, 161, 488, 192))
+        if slot == 2:
+            return self.ImageToString((161, 192, 488, 221))
+        if slot == 3:
+            return self.ImageToString((161, 221, 488, 250))
+        if slot == 4:
+            return self.ImageToString((161, 250, 488, 280))
+        else:
+            return None
+    
+    def ImageToString(self, area):
+        mainStatImage = self.equipmentImage.crop(area)
+        return pytesseract.image_to_string(mainStatImage)
