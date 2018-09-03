d85e161483248312a80f44a83e8c2afb1a457cad
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-09-03 13:18:05 +0700

    Split enum classes to different files

--- GameManager.py
+++ GameManager.py
@@ -1,31 +1,8 @@
 import time
-from enum import Enum
+from Defines import GameState, ShoesSource, DailyMission
 from Screen import ScreenType
 from ButtonPositions import ButtonPositions, Button, Position
 from Equipment import Equipment
-
-class GameState(Enum):
-    NONE = 0
-    DAILY_MISSION = 1
-    PROMOTION_BATTLE = 2
-    MYSTERIOUS_SANCTUARY = 3
-    OUT_OF_SHOES = 4
-    SHOPPING = 5
-    SUMMON = 6
-
-class ShoesSource(Enum):
-    DAILY_MISSION_REWARD = 0
-    MAIL_BOX = 1
-    SHOP_WITH_FP = 2
-    SHOP_WITH_MOONSTONE = 3
-
-class DailyMission(Enum):
-    NONE = 0
-    POWER_UP_GUARDIAN = 1
-    SUMMON_GUARDIAN = 2
-    POWER_UP_EQUIPMENT = 3
-    DISASSEMBLY = 4
-    DEAR_FRIEND = 5
 
 class GameManager:
 
