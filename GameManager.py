3bff47c213157d106a29e2a41bdea88de6028de4
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2018-10-10 23:27:23 +0700

    Add more good equipments

--- GameManager.py
+++ GameManager.py
@@ -500,6 +500,10 @@ class GameManager:
         if screenPiece is not None and self.magicShop.DidOpenEquipment(screenPiece) == False:
             self.Log("Found 6 stars purple armor")
             return screenPiece
+        screenPiece = self.screen.Find("Shop_Equipment_Gloves_6stars_Purple.png")
+        if screenPiece is not None and self.magicShop.DidOpenEquipment(screenPiece) == False:
+            self.Log("Found 6 stars purple gloves")
+            return screenPiece
         screenPiece = self.screen.Find("Shop_Equipment_Necklace_6stars_Purple.png")
         if screenPiece is not None and self.magicShop.DidOpenEquipment(screenPiece) == False:
             self.Log("Found 6 stars purple necklace")
